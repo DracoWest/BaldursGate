9619e905307b4a92951cce467fa915e7d9ec1e44
DracoWest <westcom82@gmail.com>
2025-12-19 22:28:01 -0800

    Update supabaseClient.ts

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const https://ncabqfosmehmfemqmruo.supabase.co = 'https://ncabqfosmehmfemqmruo.supabase.co';
-const sb_publishable_CJofvGKeVr0RsNtGMUtIXg_BeK3Y-gm = 'sb_publishable_CJofvGKeVr0RsNtGMUtIXg_BeK3Y-gm';
+const supabaseUrl = 'https://ncabqfosmehmfemqmruo.supabase.co';
+const supabaseAnonKey = 'sb_publishable_CJofvGKeVr0RsNtGMUtIXg_BeK3Y-gm';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
